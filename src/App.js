168a60bcedad2cc4e0b0c8fc8cf62164c9f599cc
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-02-26 15:02:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import React from 'react';
-import Header from "./components/Header";
-import Technologies from "./components/Technologies";
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
+        <div className="AppWrap">
+            <header className='header'>
+                <img src="https://avatars.mds.yandex.net/i?id=0efdf3fdecbfb91aacd73a55dcd92eb005daddc2-7755770-images-thumbs&n=13" alt="bird"/>
+            </header>
+            <nav className='nav'>
+                <ul>
+                    <li>Html</li>
+                    <li>CSS</li>
+                    <li>React</li>
+                    <li>Js</li>
+                </ul>
+            </nav>
+            <div className='container'>
+                <img src="https://avatars.mds.yandex.net/i?id=e13ea30af0fcf6bd906a81b472817952961f9e6e-7947495-images-thumbs&n=13" alt="It-Kamasutra"/>
+            </div>
         </div>
     );
 }
