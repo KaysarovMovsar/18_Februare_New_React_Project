ee26fb56553952d6ee6ce5428ff7af3b7d4a12a2
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-02-26 23:37:08 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className='header'>
+                <img src="https://avatars.mds.yandex.net/i?id=0efdf3fdecbfb91aacd73a55dcd92eb005daddc2-7755770-images-thumbs&n=13" alt="bird"/>
+            </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='container'>
+            <img src="https://avatars.mds.yandex.net/i?id=e13ea30af0fcf6bd906a81b472817952961f9e6e-7947495-images-thumbs&n=13" alt="It-Kamasutra"/>
+        </div>
+    );
+}
+
+export default Profile;
