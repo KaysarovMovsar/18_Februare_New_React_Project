506a6477dea97b5516cd11aaa8a8193a18c7c1c9
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-02 12:47:12 +0300

    first commit

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPost from "./MyPosts/MyPost";
 
 const Profile = () => {
     return (
-        <div className={s.container}>
+        <div>
             <img src="https://avatars.mds.yandex.net/i?id=e13ea30af0fcf6bd906a81b472817952961f9e6e-7947495-images-thumbs&n=13" alt="It-Kamasutra"/>
             <MyPost />
         </div>
