9684023e3cc45133d72980da895de41dadb91040
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-02 15:13:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
 
 
 const App = () => {
     return (
-        <div className="AppWrap">
-            <Header />
-            <Navbar />
-            <div className='App-Wrap-container'>
-                <Profile />
-                {/*<Dialogs />*/}
-            </div>
+        <BrowserRouter>
+            <div className="AppWrap">
+                <Header />
+                <Navbar />
+                <div className='App-Wrap-container'>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                    </Routes>
 
-        </div>
+
+
+                </div>
+            </div>
+        </BrowserRouter>
+
     );
 }
 
