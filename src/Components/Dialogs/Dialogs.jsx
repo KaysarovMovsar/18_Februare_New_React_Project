96126e4a50ea5d1a82ea7474029e9fcdb2012d0a
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-04 15:01:34 +0300

    first commit

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,8 +4,20 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () =>{
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <div className={s.dialogItems}>
+               <div className={s.dialog}>Movsar</div>
+               <div className={s.dialog}>Ahmad</div>
+               <div className={s.dialog}>Iman</div>
+               <div className={s.dialog}>Bilal</div>
+               <div className={s.dialog}>Lilia</div>
+           </div>
+           <div className={s.messages}>
+               <div className={s.message}>Hello</div>
+               <div className={s.message}>How are you?</div>
+               <div className={s.message}>Yep</div>
+               <div className={s.message}>See you tomorrow</div>
+           </div>
         </div>
     )
 }
