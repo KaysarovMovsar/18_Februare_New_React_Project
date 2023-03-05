50aa94aaf4a3d8e17bedf21eff55007d966c8905
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-05 15:24:43 +0300

    first commit

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,43 +2,33 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const Dialog = (props) => {
+    const path = "/dialogs/" + props.id
+    return <div className={s.dialog}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+}
 
-const Dialogs = () =>{
+const Message = (props) =>{
+    return <div className={s.message}>{props.message}</div>
+}
+
+const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-           <div className={s.dialogItems}>
-               <div className={s.dialog}>
-                   <NavLink to="/dialogs/1">
-                   Movsar
-                   </NavLink>
-               </div>
-               <div className={s.dialog}>
-                   <NavLink to="/dialogs/2">
-                   Ahmad
-               </NavLink>
-               </div>
-               <div className={s.dialog}>
-                   <NavLink to="/dialogs/3">
-                       Iman
-               </NavLink>
-               </div>
-               <div className={s.dialog}>
-                   <NavLink to="/dialogs/4">
-                       Bilal
-               </NavLink>
-               </div>
-               <div className={s.dialog}>
-                   <NavLink to="/dialogs/5">
-                       Lilia
-               </NavLink>
-               </div>
-           </div>
-           <div className={s.messages}>
-               <div className={s.message}>Hello</div>
-               <div className={s.message}>How are you?</div>
-               <div className={s.message}>Yep</div>
-               <div className={s.message}>See you tomorrow</div>
-           </div>
+            <div className={s.dialogItems}>
+                <Dialog name='Movsar' id='1'/>
+                <Dialog name='Ahmad' id='2'/>
+                <Dialog name='Iman' id='3'/>
+                <Dialog name='Bilal' id='4'/>
+                <Dialog name='Lilia' id='5'/>
+            </div>
+            <div className={s.messages}>
+                <Message message='Hello'/>
+                <Message message='How are you?'/>
+                <Message message='Yep'/>
+                <Message message='See you tomorrow'/>
+            </div>
         </div>
     )
 }
