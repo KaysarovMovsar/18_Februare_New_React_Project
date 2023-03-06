12a5bab00ea08bec990aaabc22a3c4ded72cf521
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-06 22:40:51 +0300

    26_lesson

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,39 +1,32 @@
 import React from 'react'
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const Dialog = (props) => {
-    const path = "/dialogs/" + props.id
-    return <div className={s.dialog}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
 
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
-
-const DialogsData = [
-    {id: 1, name: 'Movsar'},
-    {id: 2, name: 'Ahmad'},
-    {id: 3, name: 'Iman'},
-    {id: 4, name: 'Bilal'},
-    {id: 5, name: 'Lilia'},
-]
-
-const dialogsElements = DialogsData.map(p => <Dialog name={p.name} id={p.id}/>)
-
-const MessagesData = [
-    {id: 1, message: 'Hello'},
-    {id: 2, message: 'How are you?'},
-    {id: 3, message: 'Yep'},
-    {id: 4, message: 'See you tomorrow'},
-    {id: 5, message: 'Have a good meal!'},
-]
-
-const messagesElements = MessagesData.map(p => <Message message={p.message}/>)
 
 const Dialogs = () => {
+
+    const DialogsData = [
+        {id: 1, name: 'Movsar'},
+        {id: 2, name: 'Ahmad'},
+        {id: 3, name: 'Iman'},
+        {id: 4, name: 'Bilal'},
+        {id: 5, name: 'Lilia'},
+    ]
+
+    const MessagesData = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yep'},
+        {id: 4, message: 'See you tomorrow'},
+        {id: 5, message: 'Have a good meal!'},
+    ]
+
+    const dialogsElements = DialogsData.map(p => <DialogItem name={p.name} id={p.id}/>)
+    const messagesElements = MessagesData.map(p => <Message message={p.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
