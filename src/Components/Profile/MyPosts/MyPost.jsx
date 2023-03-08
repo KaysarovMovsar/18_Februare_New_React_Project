df15a608996bc9011b6f0e7e267b2b7f60bea8b6
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2023-03-08 22:25:07 +0300

    first commit

--- src/Components/Profile/MyPosts/MyPost.jsx
+++ src/Components/Profile/MyPosts/MyPost.jsx
@@ -8,13 +8,18 @@ const MyPost = (props) => {
 
     const postElements = props.PostsData.map(p =>  <Post message={p.message} LikesCount={p.LikesCount}/>)
 
+    let newPost = React.createRef()
+    let addPost = () => {
+        let text = newPost.current.value
+        alert(text)
+    }
 
     return (
         <div className={s.container}>
              <div className={s.sideBar}>
                  <div>My posts</div>
-                 <textarea className={s.input} placeholder='add'></textarea><br/>
-                 <button className={s.add}>Add Post</button>
+                 <textarea ref={newPost} className={s.input} placeholder='add'></textarea><br/>
+                 <button onClick={addPost} className={s.add}>Add Post</button>
                 <div className={s.posts}>
                     {postElements}
                 </div>
